3767172d738812ec6e9c3d6aaa2afc377409fb01
Shabo-95 <Shabo-95@hotmail.com>
2023-06-06 20:16:37 +0200

    Uploading Project to github

---
+++ src/app/mock-bookings.ts
@@ -0,0 +1,25 @@
+import { Booking } from './booking';
+
+export const Bookings: Booking[] = [
+  {
+    id: 1,
+    name: 'Shabo Saliba',
+    roomNumber: 100,
+    startDate: new Date(),
+    endDate: new Date('2023-07-31'),
+  },
+  {
+    id: 2,
+    name: 'Max Musterman',
+    roomNumber: 101,
+    startDate: new Date(),
+    endDate: new Date('2021-08-31'),
+  },
+  {
+    id: 3,
+    name: 'Test User',
+    roomNumber: 102,
+    startDate: new Date(),
+    endDate: new Date('2021-09-30'),
+  },
+];
